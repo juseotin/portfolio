3ebf6394e6da99a52d56ae2b0c5dfab40a7a632f
Justin Seo <miseo@ucsd.edu>
2025-01-31 19:48:09 -0800

    lab 4**

--- global.js
+++ global.js
@@ -93,6 +93,7 @@ export async function fetchJSON(url) {
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
+    console.log(response);
     const data = await response.json();
     return data; 
     // Fetch the JSON file from the given URL
@@ -103,17 +104,22 @@ export async function fetchJSON(url) {
   }
 }
 
-export function renderProjects(project, containerElement, headingLevel = 'h2') {
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
-  const article = document.createElement('article');
-
-  article.innerHTML = `
-    <h3>${project.title}</h3>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-  `;
-
+  projects.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
     containerElement.appendChild(article);
-  }
+  })
+  
+}
+
+export async function fetchGithubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
 
 
